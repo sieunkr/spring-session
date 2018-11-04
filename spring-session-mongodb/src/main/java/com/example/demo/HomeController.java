c493c762dea56d77949e85a8559f8855cb62c833
sieunkim <sieunkr@gmail.com>
2018-11-04 17:44:13 +0900

    스프링 세션 몽고DB 테스트

---
+++ spring-session-mongodb/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String findUid(HttpSession session) {
+        return session.getId();
+    }
+
+}
